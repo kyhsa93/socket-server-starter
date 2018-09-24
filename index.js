1ff9b0d9cbc6c574e13e907ca675f4209fd6d551
Young hoon Go <kyhsa93@naver.com>
2018-09-24 21:20:39 +0900

    Add client side code and socket connection code into server

--- index.js
+++ index.js
@@ -14,7 +14,28 @@ const io = socket(server);
 app.use(compression());
 app.disable('x-powered-by');
 
-app.listen(config.port, () => {
+// route
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// connection start
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  // listen event named 'chat message'
+  socket.on('chat message', (msg) => {
+    // boradcast all connected user
+    io.emit('chat message', msg);
+  });
+
+  // user disconnected
+  socket.on('disconnect', () => {
+  console.log('user disconnected');
+  });
+});
+
+server.listen(config.port, () => {
   // eslint-disable-next-line
   console.log(`express listening on port ${config.port}`);
 });
